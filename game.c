2562e61fca7bc9f7e743ac12d74769dd578beb0c
Hastings Greer <hastings.greer@gmail.com>
2025-01-10 14:14:15 -0500

    a

--- game.c
+++ game.c
@@ -1,15 +1,38 @@
 #include "raylib.h"
 #include <math.h>
 
+
+int mandelbrot(float x, float y) {
+	float r = 0;
+	float i = 0;
+	float n = 0;
+	while (r * r + i * i < 4) {
+		if (n > 256) {
+			return 0;
+		}
+		n += 1;
+		float ra = r * r - i * i + x;
+		i = 2 * r * i + y;
+		r = ra;
+	}
+	return n;
+
+}
 int main(void)
 {
     InitWindow(800, 450, "raylib [core] example - basic window");
+    SetTargetFPS(60);
 
     int z = 0;
 
     int frames = 0;
 
     Image canvas = GenImageColor(600, 400, GREEN);
+
+    float lx = 0;
+    float ly = 0;
+    float oldx = 0;
+    float oldy = 0;
 
     
 
@@ -19,12 +42,20 @@ int main(void)
 	    frames ++;
 	    int mx = GetMouseX();
 	    int my = GetMouseY();
+	    lx = lx + mx;
+	    ly = ly + my;
+	    SetMousePosition(0, 0);
 	    for (int i = 0; i < 400; i++) {
 		    for (int j = 0; j < 600; j++) {
-			    if ((i-my ) & (j - mx)) {
-				    ImageDrawPixel(&canvas, j, i, WHITE);
+			    float a = (i - ly) / 25000.;
+			    float b = (j - lx) / 25000.;
+			    int count = 9 * mandelbrot(a, b);
+			    Color c = {count, count, count, count};
+
+			    if (count) {
+				    ImageDrawPixel(&canvas, j, i, c);
 			    } else {
-				    ImageDrawPixel(&canvas, j, i, GREEN);
+				    ImageDrawPixel(&canvas, j, i, BLACK);
 			    }
 	    
 		    }
